8bd2273bce979eba7c630047603edf6e09219b45
jarvis-codegeek <shashankjoshi.cool@gmail.com>
2021-01-25 20:45:55 +0530

    updated steps and webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,22 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //ES5 way
 
 module.exports = {
-    entry: ["./app/main.js"],
-    output: {
+  entry: ["./app/main.js"],
+  output: {
     filename: "bundle.js"
-   },
-   plugins: [
+  },
+  plugins: [
     new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: ['./js/build/*','./css/build/*']
-      })
-   ],
-   module: {
+      cleanOnceBeforeBuildPatterns: ['./js/build/*', './css/build/*']
+    })
+  ],
+  module: {
     rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      loader: 'babel-loader',
-    }
-   ]
- }
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      }
+    ]
   }
+}
